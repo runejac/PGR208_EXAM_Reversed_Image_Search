350e50d56355febb4d117a6ea70fee86ace228b9
Joachim AC <joacchri@gmail.com>
2022-04-07 14:12:00 +0200

    added imagepicker 3rd party lib

--- app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
+++ app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
@@ -3,24 +3,29 @@ package com.example.eksamen_pgr208
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageFromGallery = findViewById<ImageView>(R.id.testImage)
+        imageFromGallery.setOnClickListener {
+
+            ImagePicker.with(this)
+                .galleryOnly()
+                .galleryMimeTypes(arrayOf("image/*"))
+                .crop()
+                .start()
+
+        }
     }
 
-    /*
-    TESTING THIS FEATURE https://developer.android.com/about/versions/13/features/photopicker#kotlin
 
-    val imageFromGallery = findViewById<ImageView>(R.id.imageview)
-    imageFromGallery.setOnClickListener(openGallery())
 
-     public void openGallery() {
-        val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
-        intent.type = "images/*" */
-        startActivityForResult(intent, PHOTO_PICKER_REQUEST_CODE)
-     } */
+
+
 
 
 }
