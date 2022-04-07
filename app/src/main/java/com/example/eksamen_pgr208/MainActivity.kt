8ad549767db60cb317298b18ca98c0cdc9f3027a
Joachim AC <joacchri@gmail.com>
2022-04-07 14:29:31 +0200

    now adding an image actually works

--- app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
+++ app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.example.eksamen_pgr208
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.github.dhaval2404.imagepicker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var imageFromGallery:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageFromGallery = findViewById<ImageView>(R.id.testImage)
-        imageFromGallery.setOnClickListener {
 
+        imageFromGallery = findViewById(R.id.testImage)
+        imageFromGallery.setOnClickListener {
             ImagePicker.with(this)
                 .galleryOnly()
                 .galleryMimeTypes(arrayOf("image/*"))
                 .crop()
                 .start()
-
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-
-
-
+        if (resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+            imageFromGallery.setImageURI(data?.data)
+        }
+    }
 
 
 }
