d7b09f7ccac203677b622a16be21455a8c917235
runejac <69790920+runejac@users.noreply.github.com>
2022-04-07 18:08:35 +0200

    added dialog (modal) for camera or gallery buttons

--- app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
+++ app/src/main/java/com/example/eksamen_pgr208/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.eksamen_pgr208
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.dhaval2404.imagepicker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var imageFromGallery:ImageView
+    private lateinit var imageFromCameraOrMemory : ImageView
 
     //TODO: Sette opp FAN (https://github.com/amitshekhariitbhu/Fast-Android-Networking)
     //      Teste endpoints i Postman
@@ -20,35 +23,77 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageFromCameraOrMemory = findViewById(R.id.addedImageFromEitherCameraOrMemory)
+        val btnGalleryOrCameraDialog : ImageButton = findViewById(R.id.btn_gallery_or_camera)
 
-        imageFromGallery = findViewById(R.id.addedImage)
-        val galleryBtn = findViewById<ImageButton>(R.id.galleryBtn)
-        val cameraBtn = findViewById<ImageButton>(R.id.cameraBtn)
+        btnGalleryOrCameraDialog.setOnClickListener {
+            println("Hello from gallery or camera button")
+            showCameraAndGalleryDialog()
+        }
 
-        galleryBtn.setOnClickListener {
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                val uri : Uri = data?.data!!
+                println(uri.toString())
+            }
+            ImagePicker.RESULT_ERROR -> {
+                Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        /*if (resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+            imageFromCameraOrMemory.setImageURI(data?.data)
+        }*/
+    }
+
+
+    // shows dialog (modal) to prompt the user to either choose camera or gallery
+    private fun showCameraAndGalleryDialog() {
+        val camOrGallDialog = Dialog(this)
+        camOrGallDialog.setContentView(R.layout.dialog_camera_or_gallery)
+        camOrGallDialog.setTitle("Choose source: ")
+        val btnGallery : ImageButton = camOrGallDialog.findViewById(R.id.btn_gallery)
+        val btnCamera : ImageButton = camOrGallDialog.findViewById(R.id.btn_camera)
+
+
+        btnGallery.setOnClickListener {
             ImagePicker.with(this)
                 .galleryOnly()
                 .galleryMimeTypes(arrayOf("image/*"))
                 .maxResultSize(400, 400)
                 .crop()
                 .start()
+
+            camOrGallDialog.dismiss()
+
+            println("gallery clicked")
         }
 
-        cameraBtn.setOnClickListener {
+        btnCamera.setOnClickListener {
             ImagePicker.with(this)
                 .cameraOnly()
                 .maxResultSize(400, 400)
                 .crop()
                 .start()
-        }
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+            camOrGallDialog.dismiss()
 
-        if (resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
-            imageFromGallery.setImageURI(data?.data)
+
+            println("camera clicked")
         }
+
+        camOrGallDialog.show()
     }
 
 
