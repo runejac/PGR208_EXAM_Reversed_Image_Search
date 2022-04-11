8b4cc573dd15814e62ab469c2e3f0049178549d3
Joachim AC <joacchri@gmail.com>
2022-04-11 11:35:42 +0200

    commented out code to add image to db, probably doesnt work, but who knows

--- app/src/main/java/com/example/eksamen_pgr208/ResultActivity.kt
+++ app/src/main/java/com/example/eksamen_pgr208/ResultActivity.kt
@@ -21,6 +21,8 @@ class ResultActivity : AppCompatActivity() {
     private var btnDeleteList : Button? = null
     private var imageResult : ImageButton? = null
 
+    // private lateinit var imageViewModel: ImageViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -35,7 +37,7 @@ class ResultActivity : AppCompatActivity() {
         // livedata ikke i bruk ennå
         // liveDataImagesList.postValue(images!!)
 
-
+        // imageViewModel = ViewModelProvider(this).get(ImageViewModel::class.java)
 
         rvImage.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
         rvImage.adapter = ResultsAdapter(this, images = ArrayList<ImageModelResultItem>(images!!))
@@ -45,12 +47,25 @@ class ResultActivity : AppCompatActivity() {
         }
 
 
-        // Vil vi her ha det sånn at når vi klikker på et bilde, så kommer det en popup
+        // Vil vi her ha det sånn at når vi klikker på et bilde, så kommer det en AlertDialog
         // med typ "Add image to database?" - Joachim
+        // Anbefales visst å laste ned DB Browser for SQLite, så kan man manuelt sjekke om data lagres i db
+
+        /*
+        * private fun addToDatabase() {
+        *   val thumbNailLink = imageResult.thumbnail_link
+        *   val imageLink = imageResult.image_link
+        *
+        *   val image = Image(0, thumbNailLink, imageLink)
+        *   imageViewModel.addImage(image)
+        *   Toast.makeText(this, "Successfully added image to database!", Toast.LENGTH_LONG).show()
+        * }
+        */
 
         /*imageResult?.setOnClickListener {
             println("image clicked")
             Toast.makeText(this,  "Image clicked!", Toast.LENGTH_LONG).show()
+            få opp en AlertDialog, trykk "Add" for addToDatabase()
         }*/
 
     }
