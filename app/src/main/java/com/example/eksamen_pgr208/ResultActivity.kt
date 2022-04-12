288473c06fd20510c4eb7b2210a850a2e858296b
Joachim AC <joacchri@gmail.com>
2022-04-12 17:31:29 +0200

    adding to db now works

--- app/src/main/java/com/example/eksamen_pgr208/ResultActivity.kt
+++ app/src/main/java/com/example/eksamen_pgr208/ResultActivity.kt
@@ -10,10 +10,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.eksamen_pgr208.adapters.ResultsAdapter
+import com.example.eksamen_pgr208.data.Image
+import com.example.eksamen_pgr208.data.ImageViewModel
 import com.example.eksamen_pgr208.data.api.ImageModelResultItem
 import kotlinx.android.synthetic.main.image_rv_layout.*
 import kotlinx.android.synthetic.main.result_activity.*
@@ -24,24 +27,23 @@ class ResultActivity : AppCompatActivity(), ResultsAdapter.RecyclerClick {
     private var btnDeleteList : Button? = null
     private var imageResult : ImageButton? = null
 
-    // private lateinit var imageViewModel: ImageViewModel
+    private lateinit var imageViewModel: ImageViewModel
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.result_activity)
-
         val images: ArrayList<ImageModelResultItem>? = intent.getParcelableArrayListExtra("images")
+
         rvImage = rv_results
         btnDeleteList = btn_delete_list
         imageResult = image_result
 
-
         // livedata ikke i bruk ennå
         // liveDataImagesList.postValue(images!!)
 
-        // imageViewModel = ViewModelProvider(this).get(ImageViewModel::class.java)
+        imageViewModel = ViewModelProvider(this)[ImageViewModel::class.java]
 
         rvImage.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
         rvImage.adapter = ResultsAdapter(this, images = ArrayList<ImageModelResultItem>(images!!), this)
@@ -50,32 +52,28 @@ class ResultActivity : AppCompatActivity(), ResultsAdapter.RecyclerClick {
             goesHomeAndClearOldSearch(images)
         }
 
-
-        // Vil vi her ha det sånn at når vi klikker på et bilde, så kommer det en AlertDialog
-        // med typ "Add image to database?" - Joachim
         // Anbefales visst å laste ned DB Browser for SQLite, så kan man manuelt sjekke om data lagres i db
-
-        /*
-        * private fun addToDatabase() {
-        *   val thumbNailLink = image.thumbnail_link
-        *   val imageLink = image.image_link
-        *
-        *   val image = Image(0, thumbNailLink, imageLink)
-        *   imageViewModel.addImage(image)
-        *   Toast.makeText(this, "Successfully added image to database!", Toast.LENGTH_LONG).show()
-        * }
-        */
     }
+
+     private fun addToDatabase(imagePos: Int) {
+         val images: ArrayList<ImageModelResultItem>? = intent.getParcelableArrayListExtra("images")
+         val thumbNailLink = images!![imagePos].thumbnail_link
+        val imageLink = images[imagePos].image_link
+        val image = Image(0, thumbNailLink, imageLink)
+        imageViewModel.addImage(image)
+         println(image)
+         Toast.makeText(this, "Successfully added image to database!", Toast.LENGTH_LONG).show()
+     }
 
     override fun onImageClick(position: Int) {
         AlertDialog.Builder(this)
-            .setTitle("What's up, my dudes?")
-            .setMessage("Do you love me?")
+            .setTitle("Hi!")
+            .setMessage("Add image to database?")
             .setPositiveButton("Yes") { dialog, _ ->
-                Toast.makeText(this, "Hello, sexy", Toast.LENGTH_SHORT).show()
+                addToDatabase(position);
             }
-            .setNegativeButton("No :((") { dialog, _ ->
-                Toast.makeText(this, "Damn you!", Toast.LENGTH_SHORT).show()
+            .setNegativeButton("No") { dialog, _ ->
+                Toast.makeText(this, "Image not added to database", Toast.LENGTH_SHORT).show()
             }
             .show()
     }
@@ -85,7 +83,5 @@ class ResultActivity : AppCompatActivity(), ResultsAdapter.RecyclerClick {
         list.clear()
         startActivity(Intent(this, MainActivity::class.java))
     }
-
-
 
 }
