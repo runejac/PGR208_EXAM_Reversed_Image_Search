626f8897373a99ea617e1daf401169da1dd9480c
StianOek <66445281+StianOek@users.noreply.github.com>
2022-04-27 13:16:35 +0200

    working with custom view ( still working )

--- app/src/main/java/com/example/eksamen_pgr208/ui/view/CustomView.kt
+++ app/src/main/java/com/example/eksamen_pgr208/ui/view/CustomView.kt
@@ -1,20 +1,18 @@
 package com.example.eksamen_pgr208.ui.view
 
-import android.R.attr.textColor
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
-import android.widget.TextView
 
 
 class CustomView : View{
 
     private val rect : Rect = Rect()
     private val paint : Paint = Paint()
+
     val textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private val SQUARE_SIZE = 100
@@ -24,19 +22,22 @@ class CustomView : View{
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
 
-    val text = "Hello"
+    val text = "Reversed Image Search"
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas!!.getClipBounds(rect)
+        val cHeight: Int = rect.height()
+        val cWidth: Int = rect.width()
+        paint.textAlign = Paint.Align.LEFT
+        paint.textSize = 80.toFloat()
+        paint.getTextBounds(text, 0, text.length, rect)
+        val x: Float = cWidth / 2f - rect.width() / 2f - rect.left
+        val y: Float = cHeight / 2f + rect.height() / 2f - rect.bottom
+        canvas.drawText(text, x, y, paint)
 
-        textPaint.color = textColor
-        canvas?.drawColor(Color.CYAN)
-        canvas?.drawRect(rect, paint)
-        rect.left = 10
-        rect.top = 10
-        rect.right = rect.left + SQUARE_SIZE
-        rect.bottom =  rect.top + SQUARE_SIZE
-        canvas?.drawText(text, 2F, 2F, textPaint)
-        paint.setColor(Color.CYAN)
+
+
+
 
 
 
